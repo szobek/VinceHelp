9ce52ea570e38a4b1c2c6d6a9347f8685b88e446
Kunszt Norbert <kunszt.norbert@gmail.com>
2023-05-02 14:55:58 +0200

    scanner + bufferedreader

--- src/vince/Main.java
+++ src/vince/Main.java
@@ -1,13 +1,43 @@
 package vince;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		String nev = "Vince";
+		String nev2 = "";
+		String nev3 = "";
 		// a String a típus.pl int=szam,char=egy karakter,stb
 		System.out.println(nev);
+		
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("név2? ");
+		try {
+			nev2 = br.readLine();
+			System.out.println(nev2);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// vagy scanner
+		Scanner s =new Scanner(System.in);
+		System.out.print("név3? ");
+		nev3=s.nextLine() ;
+		System.out.println(nev3);
+	try {
+		br.close();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}	
 		
 	}
 
