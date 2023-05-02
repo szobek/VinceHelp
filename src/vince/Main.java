1852647df52b2130b45cbfc15307ec4e75c67583
Kunszt Norbert <kunszt.norbert@gmail.com>
2023-05-02 14:47:08 +0200

    start

---
+++ src/vince/Main.java
@@ -0,0 +1,14 @@
+package vince;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String nev = "Vince";
+		// a String a típus.pl int=szam,char=egy karakter,stb
+		System.out.println(nev);
+		
+	}
+
+}
